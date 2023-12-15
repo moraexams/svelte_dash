f3ceef954b36b5d7854d6bde9acc423c95feafe7
thuvasooriya <thuvaragan28@gmail.com>
2023-12-15 15:46:58 +0530

    some changes

--- src/routes/students/+page.svelte
+++ src/routes/students/+page.svelte
@@ -5,14 +5,33 @@
 
 	const get_table = async () => {
 		// const result = await db.select().from(schema.applicants);
-		// const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1000');
-		// const data = await result.json();
 		const result = await db.query.student_tbl.findMany({
-			limit: 50,
+			limit: 10,
 			offset: 0
 		});
 		return result;
 	};
+	const headers = [
+		'Index No',
+		'Name',
+		'SGID',
+		'Medium',
+		'DRID',
+		/* 'Ranking D', */
+		'DSID',
+		'CID',
+		'NIC',
+		'Gender',
+		'School',
+		'Address',
+		'Email',
+		'Telephone',
+		'Checked',
+		'reg by',
+		'reg date',
+		'check by',
+		'check date'
+	];
 </script>
 
 <svelte:head>
@@ -29,52 +48,19 @@
 			<Table.Header>
 				<Table.Row>
 					<!-- <Table.Head class="w-[100px]">Index No</Table.Head> -->
-					<Table.Head>Index No</Table.Head>
-					<Table.Head>Name</Table.Head>
-					<Table.Head>SGID</Table.Head>
-					<Table.Head>Medium</Table.Head>
-					<Table.Head>DRID</Table.Head>
-					<!-- <Table.Head>Ranking D</Table.Head> -->
-					<Table.Head>DSID</Table.Head>
-					<Table.Head>CID</Table.Head>
-					<Table.Head>NIC</Table.Head>
-					<Table.Head>Gender</Table.Head>
-					<Table.Head>School</Table.Head>
-					<Table.Head>Address</Table.Head>
-					<Table.Head>Email</Table.Head>
-					<Table.Head>Telephone</Table.Head>
-					<Table.Head>Checked</Table.Head>
-					<Table.Head>reg by</Table.Head>
-					<Table.Head>reg date</Table.Head>
-					<Table.Head>check by</Table.Head>
-					<Table.Head>check date</Table.Head>
-					<!-- <Table.Head class="text-right">NIC</Table.Head> -->
+					{#each headers as header}
+						<Table.Head>{header}</Table.Head>
+					{/each}
 				</Table.Row>
 			</Table.Header>
 			<Table.Body>
 				{#each result as res}
-					<!-- <p>{res.name}</p> -->
 					<Table.Row>
-						<Table.Cell class="font-medium">{res.id}</Table.Cell>
-						<Table.Cell>{res.name}</Table.Cell>
-						<Table.Cell>{res.sub_grp}</Table.Cell>
-						<Table.Cell>{res.medium}</Table.Cell>
-						<Table.Cell>{res.rnk_dist_id}</Table.Cell>
-						<!-- <Table.Cell>{res.rnk_dist}</Table.Cell> -->
-						<Table.Cell>{res.exm_dist_id}</Table.Cell>
-						<Table.Cell>{res.exm_cent_id}</Table.Cell>
-						<Table.Cell>{res.nic}</Table.Cell>
-						<Table.Cell>{res.gender}</Table.Cell>
-						<Table.Cell>{res.school}</Table.Cell>
-						<Table.Cell>{res.address}</Table.Cell>
-						<Table.Cell>{res.email}</Table.Cell>
-						<Table.Cell>{res.telephone}</Table.Cell>
-						<Table.Cell>{(res.checked && 'YES') || 'NO'}</Table.Cell>
-						<Table.Cell>{res.reg_by}</Table.Cell>
-						<Table.Cell>{res.reg_dt}</Table.Cell>
-						<Table.Cell>{res.chk_by}</Table.Cell>
-						<Table.Cell>{res.chk_dt}</Table.Cell>
-						<!-- <Table.Cell class="text-right">{res.nic}</Table.Cell> -->
+						{#each Object.entries(res) as [key, value]}
+							<Table.Cell>
+								{key === 'checked' ? (value ? 'YES' : 'NO') : value}
+							</Table.Cell>
+						{/each}
 					</Table.Row>
 				{/each}
 			</Table.Body>
