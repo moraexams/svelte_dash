35f746673abee37802618919fe245f31c4cfc171
Anusan <krishnaanu200302@gmail.com>
2023-12-17 09:56:32 +0530

    Path issue fixed

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,10 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			$lib: "./src/lib"
-		  }
+			$lib: "./src/lib",
+			"@": "src/lib",
+			"@/*": "src/lib/*"
+		}
 	}
 };
 
