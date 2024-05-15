d8def00bba1201774d40a66dd452485676326255
Sahithyan <sahithyan2701@gmail.com>
2024-05-15 18:46:05 +0530

    add redirect to /dashboard

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,10 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types"
+
+export const load: PageServerLoad = async ({url, route}) => {
+	if (url.pathname === "/") {
+	
+	redirect(307, "/dashboard");
+
+	}
+}
